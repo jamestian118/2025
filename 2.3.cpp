62662525f45ac70c83bf93c474e8f48ff612ee43
tacyvan <fanzitian118@outlook.com>
2025-01-04 17:05:15 +0800

    Fix coding style for main function definition
    Updated the style of the `main` function definition by adjusting the placement of the opening brace to conform to standard styling conventions. 优化了 `main` 函数定义的代码风格，将大括号位置调整为更符合常见规范的位置。

--- 2.3.cpp
+++ 2.3.cpp
@@ -2,7 +2,8 @@
 
 // This is a single comment about our main function.
 // Our function prints text to the console.
-int main() {
+int main()
+{
     std::cout << "Hello, World" << std::endl;
     return 0;
 }
